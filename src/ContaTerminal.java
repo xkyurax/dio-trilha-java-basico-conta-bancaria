40115f02e09f738baa008d654d137f156f553106
Tiago <tiagoc684@gmail.com>
2023-08-26 23:07:38 -0300

    Entrega final do exercício.

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -4,5 +4,41 @@ public class ContaTerminal {
     public String nomeCliente;
     public double saldo;
 
+    public int getNumero() {
+        return numero;
+    }
 
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public void printConta() {
+        System.out.printf("Olá %s, obrigado por criar " +
+                "uma conta em nosso banco, sua agência é %s, " +
+                "conta %d e seu saldo %.2f já está disponível para saque.", nomeCliente, agencia, numero, saldo);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,34 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        //TO do: conhecer e importar a classe Scanner
 
+        //Exibir as mensagems para o nosso usuário
+
+        //Obter pela scanner os valores digitados no terminal
+
+        //Exibit a mensagem conta criada
+
+        Scanner sc = new Scanner(System.in);
+        ContaTerminal novaConta = new ContaTerminal();
+
+        System.out.println("Por favor, digite o número da sua conta: ");
+        novaConta.setNumero(sc.nextInt());
+
+        System.out.println("Digite o número da sua agência: ");
+        novaConta.setAgencia(sc.next());
+
+        System.out.println("Digite seu nome completo: ");
+        novaConta.setNomeCliente(sc.next());
+
+        System.out.println("Digite o valor do seu depósito: ");
+        novaConta.setSaldo(sc.nextDouble());
+
+        sc.close();
+
+        novaConta.printConta();
     }
+
 }
